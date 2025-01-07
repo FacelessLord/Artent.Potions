6c9a7edddd58e49b1bfaa1abbf2a469c89443396
faceless <skyres21@yandex.ru>
2025-01-07 16:53:53 +0500

    Berry bushes

--- src/client/java/faceless/artent/potions/client/registry/ColorProvidersRegistry.java
+++ src/client/java/faceless/artent/potions/client/registry/ColorProvidersRegistry.java
@@ -1,9 +1,16 @@
 package faceless.artent.potions.client.registry;
 
+import faceless.artent.core.math.Color;
 import faceless.artent.core.registries.IRegistry;
+import faceless.artent.potions.block.BerryBush;
 import faceless.artent.potions.blockEntities.BrewingCauldronBlockEntity;
 import faceless.artent.potions.objects.ModBlocks;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.color.block.BlockColorProvider;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockRenderView;
+import org.jetbrains.annotations.Nullable;
 
 public class ColorProvidersRegistry implements IRegistry {
   @Override
@@ -16,5 +23,66 @@ public class ColorProvidersRegistry implements IRegistry {
       }
       return 0;
     }, ModBlocks.CauldronFluid);
+
+    var grassColorTinter0 = getGrassColorTintForIndex(0);
+    var grassColorTinter1 = getGrassColorTintForIndex(1);
+
+    ColorProviderRegistry.BLOCK.register(grassColorTinter0, ModBlocks.Shadowveil);
+
+    ColorProviderRegistry.BLOCK.register(
+        (BlockState state, @Nullable BlockRenderView world, @Nullable BlockPos pos, int tintIndex) -> {
+          if (tintIndex == 0)
+            return grassColorTinter0.getColor(state, world, pos, tintIndex);
+          if (tintIndex == 1) {
+            var age = state.get(BerryBush.AGE);
+            if (age == 0)
+              return Color.White.asInt();
+            if (age == 1) {
+              var grassColor = grassColorTinter1.getColor(state, world, pos, tintIndex);
+              var greenBerryColorInitial = new Color(92, 242, 0);
+              return greenBerryColorInitial.add(Color.fromInt(grassColor)).asInt();
+            }
+            // age == 2
+            var block = state.getBlock();
+            var type = -1;
+            for (int i = 0; i < ModBlocks.berryBush.length; i++) {
+              if (block == ModBlocks.berryBush[i]) {
+                type = i;
+                break;
+              }
+            }
+
+            return switch (type) {
+              case -1 -> new Color(0, 128, 0).asInt();
+              case 0 -> new Color(50, 0, 100).asInt();
+              case 1 -> new Color(0, 50, 255).asInt();
+              case 2 -> new Color(255, 255, 0).asInt();
+              case 3 -> new Color(255, 0, 0).asInt();
+              default -> 0;
+            };
+          }
+          return Color.White.asInt();
+        },
+        ModBlocks.berryBush[0],
+        ModBlocks.berryBush[1],
+        ModBlocks.berryBush[2],
+        ModBlocks.berryBush[3]);
+  }
+
+  public static BlockColorProvider getGrassColorTintForIndex(int index) {
+    return (BlockState state, @Nullable BlockRenderView world, @Nullable BlockPos pos, int tintIndex) -> {
+      if (tintIndex != index || world == null || pos == null) {
+        return new Color(0, 50, 0).asInt();
+      }
+      var biome = world.getBiomeFabric(pos);
+      if (biome == null) return new Color(0, 50, 0).asInt();
+      var grassColor = biome.value().getGrassColorAt(pos.getX(), pos.getZ());
+      var artentColor = Color.fromInt(grassColor);
+      float[] hsv = new float[3];
+      java.awt.Color.RGBtoHSB(artentColor.red, artentColor.green, artentColor.blue, hsv);
+      hsv[1] *= 1.5f;
+
+      return java.awt.Color.HSBtoRGB(hsv[0], hsv[2], hsv[2]);
+    };
   }
 }
--- src/main/java/faceless/artent/potions/block/BerryBush.java
+++ src/main/java/faceless/artent/potions/block/BerryBush.java
@@ -3,19 +3,20 @@ package faceless.artent.potions.block;
 import faceless.artent.core.item.INamed;
 import faceless.artent.potions.ingridients.Ingredients;
 import faceless.artent.potions.objects.ModItems;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Fertilizable;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.StringIdentifiable;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
@@ -23,15 +24,27 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
+import net.minecraft.world.block.WireOrientation;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Locale;
 
 public class BerryBush extends Block implements INamed, Fertilizable {
   public final int type;
   public static final IntProperty AGE = IntProperty.of("age", 0, 2);
+  public static final EnumProperty<BushType> BUSH_TYPE = EnumProperty.of(
+      "bush_type",
+      BushType.class,
+      BushType.Single,
+      BushType.Bottom,
+      BushType.Middle,
+      BushType.Top);
 
   public BerryBush(int type, Settings settings) {
     super(settings);
     this.type = type;
     this.setDefaultState(this.stateManager.getDefaultState().with(AGE, 0));
+    this.setDefaultState(this.stateManager.getDefaultState().with(BUSH_TYPE, BushType.Single));
   }
 
   @Override
@@ -42,6 +55,7 @@ public class BerryBush extends Block implements INamed, Fertilizable {
   @Override
   protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
     builder.add(AGE);
+    builder.add(BUSH_TYPE);
   }
 
   @Override
@@ -87,7 +101,7 @@ public class BerryBush extends Block implements INamed, Fertilizable {
 
   @Override
   public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-    return SHAPE;
+    return SHAPE; // TODO
   }
 
   @Override
@@ -104,5 +118,76 @@ public class BerryBush extends Block implements INamed, Fertilizable {
   public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
     if (state.get(AGE) < 2)
       world.setBlockState(pos, state.with(AGE, state.get(AGE) + 1), Block.NOTIFY_LISTENERS);
+  }
+
+  @Override
+  protected void neighborUpdate(
+      BlockState state,
+      World world,
+      BlockPos pos,
+      Block sourceBlock,
+      @Nullable WireOrientation wireOrientation,
+      boolean notify) {
+    super.neighborUpdate(state, world, pos, sourceBlock, wireOrientation, notify);
+    var top = world.getBlockState(pos.up());
+    var bottom = world.getBlockState(pos.down());
+    var hasTopBush = top.getBlock() instanceof BerryBush;
+    var hasBottomBush = bottom.getBlock() instanceof BerryBush;
+
+    if (hasTopBush) {
+      if (hasBottomBush)
+        world.setBlockState(pos, state.with(BUSH_TYPE, BushType.Middle));
+      else
+        world.setBlockState(pos, state.with(BUSH_TYPE, BushType.Bottom));
+    } else {
+      if (hasBottomBush)
+        world.setBlockState(pos, state.with(BUSH_TYPE, BushType.Top));
+      else
+        world.setBlockState(pos, state.with(BUSH_TYPE, BushType.Single));
+    }
+  }
+
+  protected boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
+    if (floor.isIn(BlockTags.DIRT) || floor.isOf(Blocks.FARMLAND))
+      return true;
+    if (floor.getBlock() instanceof BerryBush) {
+      var count = 1;
+      for (int i = 0; i < 3; i++) {
+        if (world.getBlockState(pos.down(i + 1)).getBlock() instanceof BerryBush) {
+          count++;
+        } else break;
+      }
+
+      return count < 3;
+    }
+    return false;
+  }
+
+  @Override
+  protected boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+    BlockPos blockPos = pos.down();
+    return this.canPlantOnTop(world.getBlockState(blockPos), world, blockPos);
+  }
+
+  @Override
+  public void onPlaced(
+      World world,
+      BlockPos pos,
+      BlockState state,
+      @Nullable LivingEntity placer,
+      ItemStack itemStack) {
+    super.onPlaced(world, pos, state, placer, itemStack);
+  }
+
+  public static enum BushType implements StringIdentifiable {
+    Single,
+    Bottom,
+    Middle,
+    Top;
+
+    @Override
+    public String asString() {
+      return this.name().toLowerCase(Locale.ROOT);
+    }
   }
 }
